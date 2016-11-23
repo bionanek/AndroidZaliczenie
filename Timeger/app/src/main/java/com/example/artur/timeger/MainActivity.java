8b3ce4f382cb2d024ee895ca2bb749fb3bb6875d
bionanek <iakub.urban@gmail.com>
2016-11-23 23:16:57 +0100

    jk

--- Timeger/app/src/main/java/com/example/artur/timeger/MainActivity.java
+++ Timeger/app/src/main/java/com/example/artur/timeger/MainActivity.java
@@ -14,7 +14,7 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    //Robimy git program
+    // Robimy git program
     // drugi git
     // trzeci
 
