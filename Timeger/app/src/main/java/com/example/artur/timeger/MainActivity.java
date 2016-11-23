0a08f26fdf4a671e7084e34b2737b13934c9433c
bionanek <iakub.urban@gmail.com>
2016-11-23 22:36:19 +0100

    MainActivity fix

--- Timeger/app/src/main/java/com/example/artur/timeger/MainActivity.java
+++ Timeger/app/src/main/java/com/example/artur/timeger/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
