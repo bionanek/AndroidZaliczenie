b2f5616704b61d2806a0ce88821e4521d6ee35ce
bionanek <iakub.urban@gmail.com>
2016-11-25 22:45:05 +0100

    SQLite DataBase and Quest update

---
+++ Timeger/app/src/main/java/com/example/artur/timeger/DataBase.java
@@ -0,0 +1,42 @@
+package com.example.artur.timeger;
+
+import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+/**
+ * Created by bionanek on 25-11-2016.
+ */
+
+public class DataBase extends Activity
+{
+    public static SQLiteDatabase timegerDB = null;
+
+    public boolean createDatabase()
+    {
+        try
+        {
+            timegerDB = this.openOrCreateDatabase("Timeger.db", MODE_PRIVATE, null);
+            timegerDB.execSQL("CREATE TABLE IF NOT EXISTS quests(" +
+                    "_id INTEGER primary key autoincrement, " +
+                    "description VARCHAR not null, " +
+                    "quest_date VARCHAR," +
+                    "alarm_date VARCHAR," +
+                    "quest_place VARCHAR," +
+                    "type VARCHAR," +
+                    "comments VARCHAR);");
+            timegerDB.execSQL("CREATE TABLE IF NOT EXISTS user (" +
+                    "_id INTEGER primary key autoincrement, " +
+                    "login VARCHAR, " +
+                    "pass VARCHAR, " +
+                    "name VARCHAR, " +
+                    "surname VARCHAR);");
+        }
+        catch (Exception ex)
+        {
+            Log.e("Timeger error", "Error Creating Database :(");
+            return false;
+        }
+        return true;
+    }
+}
--- Timeger/app/src/main/java/com/example/artur/timeger/Quest.java
+++ Timeger/app/src/main/java/com/example/artur/timeger/Quest.java
@@ -10,47 +10,68 @@ public class Quest
 {
     private String _type;
     private String _place;
+    private String _description;
+    private int _id;
 
     private Date _questDate;
     private Date _alarmDate;
 
-    private boolean _isAlarmSet;
+    private boolean _isAlarmset;
 
-    public String GetType()
+    public String getDescription()
+    {
+        return this._description;
+    }
+    public void setDescription(String description)
+    {
+        this._description = description;
+    }
+    public int getId()
+    {
+        return this._id;
+    }
+    public String getType()
     {
         return _type;
     }
-    public void SetType(String newType)
+    public void setType(String newType)
     {
         this._type = newType;
     }
-    public String GetPlace()
+    public String getPlace()
     {
         return _place;
     }
-    public void SetPlace(String newPlace)
+    public void setPlace(String newPlace)
     {
         this._place = newPlace;
     }
-    public Date GetQuestDate()
+    public Date getQuestDate()
     {
         return _questDate;
     }
-    public void SetQuestDate(Date newDate)
+    public void setQuestDate(Date newDate)
     {
         this._questDate = newDate;
     }
-    public boolean IsAlarmSet()
+    public boolean IsAlarmset()
     {
-        return _isAlarmSet;
+        return _isAlarmset;
     }
-    public Date GetAlarmDate()
+    public Date getAlarmDate()
     {
         return _alarmDate;
     }
-    public void SetAlarm(Date newAlarmDate)
+    public void setAlarm(Date newAlarmDate)
     {
         this._alarmDate = newAlarmDate;
-        this._isAlarmSet = true;
+        this._isAlarmset = true;
+    }
+    @Override
+    public String toString(){
+        return _type + ", "
+                + _place + ", "
+                + _description + ", "
+                + _questDate;
     }
 }
